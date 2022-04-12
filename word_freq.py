99b9a1e5fc45ee0c614d5d13f9d2158f7c8d34e9
kimjihun2 <wlgns5674@naver.com>
2022-04-12 23:10:51 +0900

    file is uploaded

---
+++ word_freq.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+filename = input("")
+number = int(input(""))
+
+D = {}
+
+with open(filename, 'r') as fr:
+	for line in fr:
+		line = line.replace('.',' ').replace(',',' ').replace('!',' ').replace('?',' ').replace('@',' ').replace('#',' ').replace('$',' ').replace('%',' ').replace('^',' ').replace('&',' ').replace('*',' ').replace('(',' ').replace(')',' ').replace('-',' ').replace('_',' ').replace('+',' ').replace('=',' ').replace('{',' ').replace('}',' ').replace('[',' ').replace(']',' ').replace(':',' ').replace(';',' ').replace("'",' ').replace('"',' ').replace('<',' ').replace('>',' ').replace('/',' ')
+		for word in line.lower().split():	
+			if word in D:
+				D[word] += 1
+			else:
+				D[word] = 1	
+			 
+D_list = sorted(D.items(), key=lambda x: x[1], reverse=True)
+
+print(D_list[:10]) 
+	
